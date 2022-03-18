5544b4bccd83f862c1c60253f6a86c2db89b08f6
MD Rashid Hussain <64685317+m3rashid@users.noreply.github.com>
2022-03-18 20:20:59 +0530

    Time: 3 ms (35.25%), Space: 6.3 MB (79.86%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+public:
+    bool isValid(string s) {
+        stack<char> paren;
+        for (char& c : s) {
+            switch (c) {
+                case '(': 
+                case '{': 
+                case '[': paren.push(c); break;
+                case ')': if (paren.empty() || paren.top()!='(') return false; else paren.pop(); break;
+                case '}': if (paren.empty() || paren.top()!='{') return false; else paren.pop(); break;
+                case ']': if (paren.empty() || paren.top()!='[') return false; else paren.pop(); break;
+                default: ; // pass
+            }
+        }
+        return paren.empty() ;
+    }
+};
